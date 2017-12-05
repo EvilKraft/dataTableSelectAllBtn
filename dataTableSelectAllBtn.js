b1e344eaad1b9ae36aa15e91b178219a34a2130c
Konstantin Kaluzhnikov <EvilKraft@users.noreply.github.com>
2017-12-05 11:44:07 +0400

    Update dataTableSelectAllBtn.js

--- dataTableSelectAllBtn.js
+++ dataTableSelectAllBtn.js
@@ -2,9 +2,10 @@
  * Created by Kraft on 02.09.2016.
  */
 jQuery.fn.dataTable.ext.buttons.select_all = {
-	text: function ( dt ) {
-		return dt.i18n( 'buttons.selectAll', 'Select All');
-	},
+    text: '<i class="fa fa-square-o" aria-hidden="true"></i>',
+    titleAttr: function ( dt ) {
+        return dt.i18n( 'buttons.selectAll', 'Select All');
+    },
 	action: function ( e, dt, node, config ) {
 		var count_all      = dt.rows().data().length;
 		var count_selected = dt.rows({selected: true}).data().length;
@@ -25,9 +26,15 @@ jQuery.fn.dataTable.ext.buttons.select_all = {
 				var count_selected = that.rows({selected: true}).count();
 
 				if(count_selected == count_all){
-					that.text(that.i18n('buttons.selectNone', 'Deselect all'));
-				}else{
-					that.text(that.i18n('buttons.selectAll', 'Select All'));
+					that.text('<i class="fa fa-check-square-o" aria-hidden="true"></i>');
+                    that.titleAttr = that.i18n('buttons.selectNone', 'Deselect all');
+				}else if(count_selected > 0){
+                    that.text('<i class="fa fa-minus-square-o" aria-hidden="true"></i>');
+                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
+				}
+				else{
+                    that.text('<i class="fa fa-square-o" aria-hidden="true"></i>');
+                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
 				}
 			}
 		});
@@ -35,9 +42,10 @@ jQuery.fn.dataTable.ext.buttons.select_all = {
 };
 
 jQuery.fn.dataTable.ext.buttons.select_all_current = {
-	text: function ( dt ) {
-		return dt.i18n( 'buttons.selectAll', 'Select All');
-	},
+    text: '<i class="fa fa-square-o" aria-hidden="true"></i>',
+    titleAttr: function ( dt ) {
+        return dt.i18n( 'buttons.selectAll', 'Select All');
+    },
 	action: function ( e, dt, node, config ) {
 		var count_all      = dt.rows({page:'current'}).data().length;
 		var count_selected = dt.rows({page:'current', selected: true}).data().length;
@@ -57,11 +65,17 @@ jQuery.fn.dataTable.ext.buttons.select_all_current = {
 				var count_all      = that.rows({page:'current'}).count();
 				var count_selected = that.rows({page:'current', selected: true}).count();
 
-				if(count_selected == count_all){
-					that.text(that.i18n('buttons.selectNone', 'Deselect all'));
-				}else{
-					that.text(that.i18n('buttons.selectAll', 'Select All'));
-				}
+                if(count_selected == count_all){
+                    that.text('<i class="fa fa-check-square-o" aria-hidden="true"></i>');
+                    that.titleAttr = that.i18n('buttons.selectNone', 'Deselect all');
+                }else if(count_selected > 0){
+                    that.text('<i class="fa fa-minus-square-o" aria-hidden="true"></i>');
+                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
+                }
+                else{
+                    that.text('<i class="fa fa-square-o" aria-hidden="true"></i>');
+                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
+                }
 			}
 		});
 	}
