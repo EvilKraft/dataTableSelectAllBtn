b06a6ef4f1d483fe3f55452b351e41588c209a51
Konstantin Kaluzhnikov <EvilKraft@users.noreply.github.com>
2020-02-25 17:29:04 +0400

    Update dataTableSelectAllBtn.js

--- dataTableSelectAllBtn.js
+++ dataTableSelectAllBtn.js
@@ -1,11 +1,15 @@
 /**
  * Created by Kraft on 02.09.2016.
  */
+let style = document.createElement('style');
+style.innerHTML = `
+  .dtRowSelectNone::before   {font-family: "Font Awesome 5 Free"; font-size: 1.33em; font-weight: 400; content: "\\f0c8";}
+  .dtRowSelectNotAll::before {font-family: "Font Awesome 5 Free"; font-size: 1.33em; font-weight: 400; content: "\\f146";}
+  .dtRowSelectAll::before    {font-family: "Font Awesome 5 Free"; font-size: 1.33em; font-weight: 400; content: "\\f14a";}
+`;
+document.head.appendChild(style);
+
 jQuery.fn.dataTable.ext.buttons.select_all = {
-    text: '<i class="fa fa-square-o" aria-hidden="true"></i>',
-    titleAttr: function ( dt ) {
-        return dt.i18n( 'buttons.selectAll', 'Select All');
-    },
 	action: function ( e, dt, node, config ) {
 		var count_all      = dt.rows().data().length;
 		var count_selected = dt.rows({selected: true}).data().length;
@@ -22,19 +26,19 @@ jQuery.fn.dataTable.ext.buttons.select_all = {
 		// .DT namespace listeners are removed by DataTables automatically on table destroy
 		dt.on( 'draw.dt.DT select.dt.DT deselect.dt.DT', function () {
 			if ( that.select.items() === 'row' ) {
-				var count_all      = that.rows().count();
-				var count_selected = that.rows({selected: true}).count();
+				let count_all      = that.rows().count();
+				let count_selected = that.rows({selected: true}).count();
 
 				if(count_selected == count_all){
-					that.text('<i class="fa fa-check-square-o" aria-hidden="true"></i>');
-                    that.titleAttr = that.i18n('buttons.selectNone', 'Deselect all');
+					that.text('<span class="dtRowSelectAll"></span>');
+					that[0].node.setAttribute('title', that.i18n('buttons.selectNone', 'Deselect all'));
 				}else if(count_selected > 0){
-                    that.text('<i class="fa fa-minus-square-o" aria-hidden="true"></i>');
-                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
+					that.text('<span class="dtRowSelectNotAll"></span>');
+					that[0].node.setAttribute('title', that.i18n('buttons.selectAll', 'Select All'));
 				}
 				else{
-                    that.text('<i class="fa fa-square-o" aria-hidden="true"></i>');
-                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
+					that.text('<span class="dtRowSelectNone"></span>');
+					that[0].node.setAttribute('title', that.i18n('buttons.selectAll', 'Select All'));
 				}
 			}
 		});
@@ -42,10 +46,6 @@ jQuery.fn.dataTable.ext.buttons.select_all = {
 };
 
 jQuery.fn.dataTable.ext.buttons.select_all_current = {
-    text: '<i class="fa fa-square-o" aria-hidden="true"></i>',
-    titleAttr: function ( dt ) {
-        return dt.i18n( 'buttons.selectAll', 'Select All');
-    },
 	action: function ( e, dt, node, config ) {
 		var count_all      = dt.rows({page:'current'}).data().length;
 		var count_selected = dt.rows({page:'current', selected: true}).data().length;
@@ -65,17 +65,17 @@ jQuery.fn.dataTable.ext.buttons.select_all_current = {
 				var count_all      = that.rows({page:'current'}).count();
 				var count_selected = that.rows({page:'current', selected: true}).count();
 
-                if(count_selected == count_all){
-                    that.text('<i class="fa fa-check-square-o" aria-hidden="true"></i>');
-                    that.titleAttr = that.i18n('buttons.selectNone', 'Deselect all');
-                }else if(count_selected > 0){
-                    that.text('<i class="fa fa-minus-square-o" aria-hidden="true"></i>');
-                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
-                }
-                else{
-                    that.text('<i class="fa fa-square-o" aria-hidden="true"></i>');
-                    that.titleAttr = that.i18n('buttons.selectAll', 'Select All');
-                }
+				if(count_selected == count_all){
+					that.text('<span class="dtRowSelectAll"></span>');
+					that[0].node.setAttribute('title', that.i18n('buttons.selectNone', 'Deselect all'));
+				}else if(count_selected > 0){
+					that.text('<span class="dtRowSelectNotAll"></span>');
+					that[0].node.setAttribute('title', that.i18n('buttons.selectAll', 'Select All'));
+				}
+				else{
+					that.text('<span class="dtRowSelectNone"></span>');
+					that[0].node.setAttribute('title', that.i18n('buttons.selectAll', 'Select All'));
+				}
 			}
 		});
 	}
